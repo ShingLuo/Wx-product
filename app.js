043db5438f6f2e48d313174550a5640043733785
ZhouHepeng <Zhou-Hepeng@qq.com>
2017-07-31 19:06:40 +0800

    微信小程序

---
+++ app.js
@@ -0,0 +1,43 @@
+//app.js
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    this.ajaxurl = 'https://product.360che.com/'
+  },
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null,
+    //显示loading
+    showLoading: function (name) {
+      return wx.showToast({
+        title: name,
+        icon: 'loading'
+      });
+    },
+    //关闭loading
+    hideLoading: function () {
+      return wx.hideToast();
+    }
+  },
+
+})
